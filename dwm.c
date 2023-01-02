e030b3615f7f88ee216a4a569bb6ae4b28a3159a
Richard Cook <royaltycook@gmail.com>
2023-01-02 00:33:16 +0000

    Add patch for slow resize / move when in floating mode

--- dwm.c
+++ dwm.c
@@ -1332,7 +1332,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 288))
 				continue;
 			lasttime = ev.xmotion.time;
 
