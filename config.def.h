7fa91f4fe2f8acee18c0b384a59c607ca85abac9
Richard Cook <royaltycook@gmail.com>
2023-01-08 01:50:15 +0000

    Add patches:
    fibonacci
    centremaster
    stacker
    swallow
    scratchpad
    
    Also revert centretitle patch

--- config.def.h
+++ config.def.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. **/
 #include <X11/XF86keysym.h>
-
+#include "gaplessgrid.c"
+#include "fibonacci.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -10,10 +11,11 @@ static const unsigned int systrayonleft = 0;    /* 0: systray in the right corne
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=11" };
-static const char dmenufont[]       = "JetBrains Mono:size=11";
+static const char *fonts[]          = { "JetBrains Mono:size=9" };
+static const char dmenufont[]       = "JetBrains Mono:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -33,9 +35,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+        /* class                   instance  title           tags mask   isfloating  isterminal   noswallow  monitor */
+        { "jetbrains-clion",       NULL,     NULL,           1 << 2,     0,          0,           -1,       -1 },
+        { "jetbrains-idea",        NULL,     NULL,           1 << 1,     0,          0,           -1,       -1 },
+        { "leagueclientux.exe",    NULL,     NULL,           1,          1,          0,           -1,       -1 },
+        { "league of legends.exe", NULL,     NULL,           1,          1,          0,           -1,       -1 },
+        { "trackmania.exe",        NULL,     NULL,           1,          1,          0,           -1,       -1 },
+        { "St",                    NULL,     NULL,           0,          0,          1,           0,        -1 },
+	{ NULL,                    NULL,     "Event Tester", 0,          0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -44,11 +51,17 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+        { "###",      gaplessgrid },
+        { "[@]",      spiral },
+ 	{ "[\\]",     dwindle },
+        { "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* Add somewhere in your constants definition section */
@@ -63,33 +76,49 @@ static const char *mutevol[] = { "/usr/bin/amixer", "set",   "Master", "toggle",
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+#define STACKKEYS(MOD,ACTION) \
+	{ MOD, XK_j,         ACTION##stack, {.i = INC(+1) } }, \
+	{ MOD, XK_k,         ACTION##stack, {.i = INC(-1) } }, \
+	{ MOD, XK_backslash, ACTION##stack, {.i = PREVSEL } }, \
+	{ MOD, XK_s,         ACTION##stack, {.i = 0 } }, \
+	{ MOD, XK_a,         ACTION##stack, {.i = 1 } }, \
+	{ MOD, XK_z,         ACTION##stack, {.i = 2 } }, \
+	{ MOD, XK_x,         ACTION##stack, {.i = -1 } },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-F", "-c", "-l", "15", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-    { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-//    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+        { MODKEY,                       XK_c,      togglescratch,  {.v = scratchpadcmd } },
+//    	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	STACKKEYS(MODKEY,                          focus)
+	STACKKEYS(MODKEY|ShiftMask,                push)
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_space, zoom,           {0} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-    { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+        { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+        { MODKEY,                       XK_r,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[5]} },
+        { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[7]} },
 //	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
